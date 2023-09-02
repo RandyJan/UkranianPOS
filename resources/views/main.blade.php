bb6527cc64b9b14a0cb115e870044c0418c47d86
Randy Jan <randyjan250@gmail.com>
2023-09-02 15:54:06 -0700

    Done UI buttons,input and table

---
+++ resources/views/main.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel='stylesheet' href=
+    "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="{{'css/style.css'}}">
+
+</head>
+<body>
+    <div class="tableContainer">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Items</th>
+            <th scope="col">Price</th>
+            <th scope="col">QTY</th>
+            <th scope="col">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
+    <br>
+
+
+  <div class="mycontainer">
+    <div class="inputContainer" style="">
+
+    <input class="input is-normal mt-3 is-black" type="text" placeholder="Normal input">
+</div>
+        <div class="buttons is-centered">
+
+            <button class="button is-warning btn-lg"> 1</button>
+            <button class="button is-warning btn-lg"> 2</button>
+            <button class="button is-warning btn-lg"> 3</button>
+            <button class="button is-danger btn-lg">Clear </button>
+            <button class="button is-dark btn-lg">Sub total </button>
+
+            <button class="button is-success btn-lg">Void </button>
+            <button class="button is-warning btn-lg"> 4</button>
+            <button class="button is-warning btn-lg"> 5</button>
+            <button class="button is-warning btn-lg"> 6</button>
+            <button class="button is-black btn-lg"> Open drawer</button>
+            <button class="button is-dark btn-lg"> User</button>
+            <button class="button is-success btn-lg"> Void all</button>
+            <button class="button is-warning btn-lg"> 7</button>
+            <button class="button is-warning btn-lg"> 8</button>
+            <button class="button is-warning btn-lg"> 9</button>
+            <button class="button is-black btn-lg"> Print reciept</button>
+            <button class="button is-link btn-lg"> All stop</button>
+
+            <button class="button is-link btn-lg">All auth </button>
+            <button class="button is-warning btn-lg"> 0</button>
+            <button class="button is-warning btn-lg"> 00</button>
+            <button class="button is-warning btn-lg"> .</button>
+            <button class="button is-info btn-lg"> Safe drop</button>
+            <button class="button is-success btn-lg"> Puregold disc</button>
+            <button class="button is-danger btn-lg"> Preset</button>
+
+
+        </div>
+
+    </div>
+</body>
+
+</html>
